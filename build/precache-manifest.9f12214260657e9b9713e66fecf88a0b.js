a4347a470274939edf13b18121f4eb7a0587f4bb
Sankalpa Fernando <sankalpafernando2017@gmail.com>
2020-10-12 17:01:00 +0530

    Init Build Version

---
+++ build/precache-manifest.9f12214260657e9b9713e66fecf88a0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266a3bcdd77c73a89cf5e2538ca0eb88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c425e8a363443024b779",
+    "url": "/static/css/2.f7e6c76a.chunk.css"
+  },
+  {
+    "revision": "fed6d84761245e052137",
+    "url": "/static/css/main.59182e7c.chunk.css"
+  },
+  {
+    "revision": "c425e8a363443024b779",
+    "url": "/static/js/2.34a645db.chunk.js"
+  },
+  {
+    "revision": "664edcf1e03a7581cf8234ad9df15e41",
+    "url": "/static/js/2.34a645db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fed6d84761245e052137",
+    "url": "/static/js/main.a5d40a2f.chunk.js"
+  },
+  {
+    "revision": "7207d21bd28df87dd090",
+    "url": "/static/js/runtime-main.38f6d5db.js"
+  }
+]);
